207870bec430e6d09636c5d383a22d933c239b5c
sanjay555 <sanjayshr555@gmail.com>
2018-01-30 03:21:54 +0530

    saved to DB

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const axios = require('axios')
+const mongoose = require('mongoose')
+const dbConfig = require('./config/db.config')
+
+mongoose.connect(dbConfig.url)
+
+// Schema/Model
+const Symbol = mongoose.model('Symbol', {Symbol: String})
+
 var data = [];
+
 axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=5').then((response) =>  {
-  console.log(response.data[1].symbol);
+  // console.log(response.data[1].symbol);
   // for(var i = 0; i < response.data.length; i++) {
   //   data.push(response.data[i].symbol)
   // }
-  response.data.forEach((e) => console.log(e.symbol))
-  console.log(data);
+  response.data.forEach((e) => {
+    data.push(e.symbol)
+  })
+  console.log(data[0]);
+  const allSymbols =  new Symbol({Symbol: data[0]})
+
+  // Save to DB
+  allSymbols.save().then(() => console.log('Saved'))
 })
